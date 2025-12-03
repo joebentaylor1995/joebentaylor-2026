05db7b4a5ed48a31934030b786c62a259e42be72
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2025-12-03 23:45:47 +0000

    ✨ Added Real data
    Type: feature
    Build: ⏩ Skipped

--- app/queries/getHome.js
+++ app/queries/getHome.js
@@ -1,56 +1,17 @@
 export const GET_HOME = `
     query {
-        seo: homePage {
-            seoMeta {
-                title
-                desc: description
+        home {
+            subheading
+            title
+            videoThumbnail {
+                url
+                id
             }
-        }
-
-        page: homePage {
-            pageTitle
-            subtext
-
-            ethosText
-
-            strategyTitle
-            insightsTitle
-            evaluateTitle
-            strategyText {
-                value
-                blocks
-                links
-                __typename
+            video {
+                url
+                provider
             }
-            insightsText {
-                value
-                blocks
-                links
-                __typename
-            }
-            evaluateText {
-                value
-                blocks
-                links
-                __typename
-            }
-
-            caseStudiesTitle
-
-            teamTitle
-            teamImage {
-                alt
-                responsiveImage(imgixParams: { fit: fill, w: 720, h: 900, dpr: 2 }) {
-                    alt
-                    src
-                    srcSet
-                    width
-                    height
-                    aspectRatio
-                    sizes
-                }
-                blurUpThumb
-            }
+            unicornScene
         }
     }
 `;
