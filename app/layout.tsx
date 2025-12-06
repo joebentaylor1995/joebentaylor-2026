b43f0c61dba42af27b6ac0eb4d893a615184bca2
Joe Taylor <54585427+joebentaylor1995@users.noreply.github.com>
2025-12-06 01:51:55 +0000

    ✨ Favicon Added
    Type: feature
    Build: ⏩ Skipped

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,30 +3,44 @@
 import '@/theme/tackl/waffl/WebComponent';
 import Client from './Client';
 import Server from './Server';
-import type { Viewport } from "next";
+import type { Viewport, Metadata } from 'next';
 
 // Styles
 // ------------
 import '@css/global.css';
 
 export const viewport: Viewport = {
-    width: "device-width",
-    initialScale: 1,
-    viewportFit: "cover",
+	width: 'device-width',
+	initialScale: 1,
+	viewportFit: 'cover',
 };
 
-
+export const metadata: Metadata = {
+	appleWebApp: {
+		title: 'JBT',
+	},
+	icons: {
+		icon: [
+			{ url: '/icon1.png', sizes: '32x32', type: 'image/png' },
+			{ url: '/icon0.svg', type: 'image/svg+xml' },
+			{ url: '/favicon.ico', sizes: 'any' },
+		],
+		apple: [
+			{ url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
+		],
+	},
+	manifest: '/manifest.json',
+};
 
 // Component
 // ------------
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <Client>
-            <Server>{children}</Server>
-        </Client>
-    );
+	return (
+		<Client>
+			<Server>{children}</Server>
+		</Client>
+	);
 };
-
 
 // DisplayName added for better debugging in React DevTools
 RootLayout.displayName = 'RootLayout';
